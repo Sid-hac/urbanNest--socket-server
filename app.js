bfd94d9e36747c7fa3294bb0ccd953c18d42ab7f
sid <sidengineer.014@gmail.com>
2024-06-18 23:34:17 +0530

    chat functionality added socket.io

---
+++ app.js
@@ -0,0 +1,53 @@
+
+import {Server} from "socket.io"
+
+const io = new Server({
+    cors:{
+        origin:"http://localhost:5173"
+    }
+})
+
+  let onlineUsers = [];
+ 
+  const addUser = (userId , socketId) => {
+      
+    const userExists = onlineUsers.find(user => user.userId === userId)
+    if(!userExists){
+        onlineUsers.push({
+            userId,
+            socketId
+        })
+    }
+  }
+
+  const removeUser = (socketId) => {
+       
+       onlineUsers = onlineUsers.filter(user => user.socketId !== socketId)
+  }
+
+  const getUser = (userId) => {
+    return onlineUsers.find(user => user.userId === userId)
+}
+
+
+io.on("connection",(socket)=>{
+    
+    socket.on("newUser" , (userId) =>{
+       addUser(userId, socket.id);
+       console.log(onlineUsers);
+    })
+
+    socket.on("newMessage", ({receiverId , data}) => {
+        const user = getUser(receiverId);
+        if(user){
+            io.to(user.socketId).emit("getMessage" , data)
+        }
+    })
+
+    socket.on("disconnect" , () => {
+        removeUser(socket.id);
+    })
+
+});
+
+io.listen("4000")
